276a29aec41d11ef5275386c02808bafd0c4c640
MKJ <mkj.aldrin@icloud.com>
2024-06-02 13:09:32 +0200

    derived extends signal

--- src/signal.js
+++ src/signal.js
@@ -1,68 +1,79 @@
 /**@typedef {()=>void} Context */
 /**@typedef {string} ContextID */
 
+/**@type {Set<[Context,ContextID]>} */
+const allContexts = new Set();
+
 /**
-* @template T
-*/
+ * @template T
+ */
 export class Signal {
   /** @type {T} */
-  #value
+  #value;
 
   /**@type {Set<[Context,ContextID]>} */
-  #context_references = new Set()
+  #context_references = new Set();
 
   static group() {
     /**@type {Set<Signal>} */
-    let group = new Set()
+    let group = new Set();
 
     return {
       /**@param {Signal} signal */
       add(signal) {
-        group.add(signal)
+        group.add(signal);
       },
       clear_effects() {
-        group.forEach(s => s.#context_references.clear())
-      }
-    }
+        group.forEach((s) => s.#context_references.clear());
+      },
+    };
   }
 
   /** @param {T} init */
   constructor(init) {
-    this.#value = init
+    this.#value = init;
   }
 
   /**
-   * If set, runs all the Context referencing this Signal
+   * set: sets value and runs all the effects referencing this Signal
    */
   set value(v) {
-    this.#value = v
+    this.#value = v;
 
     if (is_batching) {
-      this.#context_references.forEach(([context]) => batch_context.add(context))
+      this.#context_references.forEach(([context]) => batch_context.add(context));
     } else {
-      this.#context_references.forEach(([context]) => context())
+      this.#context_references.forEach(([context]) => context());
     }
   }
 
-
   /**
-   * If get & called inside an effect adds the current Context to this signals references
+   *
+   * get inside effect: adds the current effect to this signals references
+   * @type {T}
    */
   get value() {
-    if (current_context) {
-      this.#context_references.add([current_context, current_context_id])
+    if (this instanceof Derived) {
+      if (allContexts.has(current_context)) {
+        return this.#value;
+      }
     }
-    return this.#value
+    if (current_context) {
+      this.#context_references.add([current_context, current_context_id]);
+      allContexts.add(current_context);
+    }
+
+    return this.#value;
   }
 
   /**
-   * Run all the referenced effects manually 
+   * Run all the referenced effects manually
    */
   signal() {
     if (is_batching) {
-      this.#context_references.forEach(([context]) => batch_context.add(context))
+      this.#context_references.forEach(([context]) => batch_context.add(context));
     } else {
-      this.#context_references.forEach(([context]) => context())
+      this.#context_references.forEach(([context]) => context());
     }
   }
 
@@ -71,18 +82,35 @@ export class Signal {
    * @param {ContextID} id
    */
   clear(id) {
-    let c = [...this.#context_references].find(([_, context_id]) => context_id == id)
-    this.#context_references.delete(c)
+    let c = [...this.#context_references].find(([_, context_id]) => context_id == id);
+    this.#context_references.delete(c);
   }
 }
 
-let current_context = undefined
-/**@type {string} */
-let current_context_id = undefined
+/**
+ * @template {()=>any} T
+ * @extends {Signal<ReturnType<T>>}
+ */
+export class Derived extends Signal {
+  /**
+   * @param {T} fn
+   */
+  constructor(fn) {
+    super();
 
-let is_batching = false
+    effect(() => {
+      this.value = fn();
+    });
+  }
+}
+
+let current_context = undefined;
+/**@type {string} */
+let current_context_id = undefined;
+
+let is_batching = false;
 /** @type {Set<Context>} */
-let batch_context = new Set()
+let batch_context = new Set();
 
 /**
  * Create an effect that runs every time the value of Signal is set
@@ -90,11 +118,11 @@ let batch_context = new Set()
  * @param {string} id
  */
 export function effect(fn, id = undefined) {
-  current_context_id = id
-  current_context = fn
-  fn()
-  current_context = undefined
-  current_context_id = undefined
+  current_context_id = id;
+  current_context = fn;
+  fn();
+  current_context = undefined;
+  current_context_id = undefined;
 }
 
 /**
@@ -102,9 +130,9 @@ export function effect(fn, id = undefined) {
  * @param {Context} fn
  */
 export function batch(fn) {
-  is_batching = true
-  fn()
-  is_batching = false
-  batch_context.forEach((context) => context())
-  batch_context.clear()
+  is_batching = true;
+  fn();
+  is_batching = false;
+  batch_context.forEach((context) => context());
+  batch_context.clear();
 }
--- test/app.js
+++ test/app.js
@@ -1,132 +1,160 @@
-import { batch, effect, Signal } from "../src/signal.js"
+import { batch, effect, Signal, Derived } from "../src/signal.js";
 
-let s_age = new Signal(0)
+let count0 = new Signal(0);
+let count1 = new Signal(0);
+//Type of dervied is based on ReturnType of the passed fn
+let product = new Derived(() => `${count0.value * count1.value}`);
 
-let s_name = new Signal("")
-/**@type {Signal<string[]>} */
-let s_list = new Signal([])
+const count0Btn = document.createElement("button");
+count0Btn.textContent = "c0";
+const count1Btn = document.createElement("button");
+count1Btn.textContent = "c1";
+const resultElement = document.createElement("div");
 
-let age_el = document.createElement("div")
-let name_el = document.createElement("div")
-let list_el = document.createElement("ul")
-
-document.body.append(age_el, name_el, list_el)
-
-function list_item(value = '', index = 0) {
-  const el = document.createElement("li")
-  el.innerHTML = `<div>${value}</div>`
-
-  let remove_button = document.createElement("button")
-  remove_button.textContent = 'rem'
-  el.appendChild(remove_button)
-
-  remove_button.onclick = () => {
-    s_list.value.splice(index, 1)
-    s_list.signal()
-  }
-
-  return el
-}
+count0Btn.addEventListener("click", (e) => {
+  count0.value++;
+});
+count1Btn.addEventListener("click", (e) => {
+  count1.value++;
+});
 
 effect(() => {
-  age_el.textContent = `age: ${s_age.value.toString()}`
-  name_el.textContent = `name: ${s_name.value}`
+  resultElement.textContent = `${count0.value} * ${count1.value}` + " = " + product.value;
+});
 
-  list_el.innerHTML = ''
-  list_el.append(...s_list.value.map((v, i) => list_item(v, i)))
+document.body.append(count0Btn, count1Btn, resultElement);
 
-})
+// let s_age = new Signal(0);
 
-let list_input = document.createElement("input")
-list_input.addEventListener("change",
-  /** @param {InputEvent & {target:HTMLInputElement}} e */ e => {
-    s_list.value.push(e.target.value)
-    s_list.signal()
-    e.target.value = ''
-  })
+// let s_name = new Signal("");
+// /**@type {Signal<string[]>} */
+// let s_list = new Signal([]);
 
-document.body.appendChild(list_input)
+// let age_el = document.createElement("div");
+// let name_el = document.createElement("div");
+// let list_el = document.createElement("ul");
 
+// document.body.append(age_el, name_el, list_el);
 
-document.body.appendChild(document.createElement("br"))
+// function list_item(value = "", index = 0) {
+//   const el = document.createElement("li");
+//   el.innerHTML = `<div>${value}</div>`;
 
-// Test for effectgroup/effectid, users should be able to update/reinit/clear an effect based on id/group 
+//   let remove_button = document.createElement("button");
+//   remove_button.textContent = "rem";
+//   el.appendChild(remove_button);
+
+//   remove_button.onclick = () => {
+//     s_list.value.splice(index, 1);
+//     s_list.signal();
+//   };
+
+//   return el;
+// }
+
+// effect(() => {
+//   age_el.textContent = `age: ${s_age.value.toString()}`;
+//   name_el.textContent = `name: ${s_name.value}`;
+
+//   list_el.innerHTML = "";
+//   list_el.append(...s_list.value.map((v, i) => list_item(v, i)));
+// });
+
+// let list_input = document.createElement("input");
+// list_input.addEventListener(
+//   "change",
+//   /** @param {InputEvent & {target:HTMLInputElement}} e */ (e) => {
+//     s_list.value.push(e.target.value);
+//     s_list.signal();
+//     e.target.value = "";
+//   }
+// );
+
+// document.body.appendChild(list_input);
+
+// document.body.appendChild(document.createElement("br"));
+
+// Test for effectgroup/effectid, users should be able to update/reinit/clear an effect based on id/group
 // This ensures that the caller of effect can manage the scope that a signal should know about
 // As in the case below, a user picks an pick_item(signal) but should only be able to pick one item and still "listen" for changes on that signal
 // We need to clear the old effect before giving the pick_item(signal) an new effect(context)
 
-let picker_button = document.createElement("button")
-picker_button.textContent = 'pick'
+let picker_button = document.createElement("button");
+picker_button.textContent = "pick";
 
 /**@type {Signal<number>} */
-let picker_signal = null
+let picker_signal = null;
 
 /**
-  * @param {MouseEvent & {target:HTMLElement}} e
-  * @this {HTMLElement}
-  */
+ * @param {MouseEvent & {target:HTMLElement}} e
+ * @this {HTMLElement}
+ */
 function picker_handler(e) {
-  if (e.target.getAttribute("data-pick") == 'element') {
-    picker_signal?.clear("picker")
+  if (e.target.getAttribute("data-pick") == "element") {
+    picker_signal?.clear("picker");
 
-    let index = +e.target.getAttribute("data-index")
-    let { s } = pick_list_signals[index]
-    picker_signal = s
-
+    let index = +e.target.getAttribute("data-index");
+    // let { s } = pick_list_signals[index];
+    let { s } = pick_list_signals.find(
+      ({ pick_element }) => +pick_element.getAttribute("data-index") == index
+    );
+    picker_signal = s;
 
     // This is where the effect is called
     // It needs to be unique
     effect(() => {
-      console.log("picked", s.value)
-    }, "picker")
+      console.log(`picked item change: idx: ${s.value}`);
+    }, "picker");
 
-    window.removeEventListener("click", bound_picker)
-    bound_picker = null
+    window.removeEventListener("click", bound_picker);
+    bound_picker = null;
   }
 }
 
 /**@type {typeof picker_handler} */
-let bound_picker = null
+let bound_picker = null;
 
-picker_button.addEventListener("click", e => {
-  window.addEventListener("click", bound_picker = picker_handler)
-})
+picker_button.addEventListener("click", (e) => {
+  window.addEventListener("click", (bound_picker = picker_handler));
+});
 
-let pick_list = document.createElement("ul")
+let pick_list = document.createElement("ul");
 
 let pick_list_signals = [...Array(4)].map((_, i) => {
-  let s = new Signal(i)
+  let s = new Signal(i);
 
-  let pick_element = document.createElement("li")
-  pick_element.setAttribute("data-pick", 'element')
+  let pick_element = document.createElement("li");
+  pick_element.setAttribute("data-pick", "element");
 
-  let text_node = document.createTextNode("")
+  let text_node = document.createTextNode("");
 
-  let remove_button = document.createElement("button")
-  remove_button.textContent = 'rem'
+  let remove_button = document.createElement("button");
+  remove_button.textContent = "rem";
+  let random_index_button = document.createElement("button");
+  random_index_button.textContent = "rand";
 
   remove_button.addEventListener("click", () => {
-    pick_element.remove()
-    pick_list_signals.splice(s.value, 1)
-    pick_list_signals.forEach(({ s }, i) => s.value = i)
-  })
+    pick_element.remove();
+    pick_list_signals.splice(s.value, 1);
+    pick_list_signals.forEach(({ s }, i) => (s.value = i));
+  });
 
-  pick_element.append(text_node, remove_button)
+  random_index_button.addEventListener("click", (e) => {
+    s.value = Math.floor(Math.random() * 999);
+  });
 
-  let id = crypto.randomUUID().replace(/-.*/, '')
+  pick_element.append(text_node, remove_button, random_index_button);
+
+  let id = crypto.randomUUID().replace(/-.*/, "");
 
   effect(() => {
-    pick_element.setAttribute("data-index", s.value.toString())
-    text_node.textContent = `pick item: ${s.value}, id: ${id}`
-  })
+    pick_element.setAttribute("data-index", s.value.toString());
+    text_node.textContent = `pick item idx: ${s.value}, id: ${id}`;
+  });
 
-  return { pick_element, s }
-})
+  return { pick_element, s };
+});
 
-pick_list.append(...pick_list_signals.map(({ pick_element }) => pick_element))
+pick_list.append(...pick_list_signals.map(({ pick_element }) => pick_element));
 
-document.body.append(picker_button, pick_list)
-
-
-
-
+document.body.append(picker_button, pick_list);
